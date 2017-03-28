0425d38b1e30020cbdb0baf2941728eb2e8e99a3
dpham147 <dannypham92@hotmail.com>
2017-03-28 14:51:57 -0700

    asd

--- app/src/main/java/edu/orangecoastcollege/cs273/scarnesdice/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/scarnesdice/MainActivity.java
@@ -11,10 +11,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView playerScoreTextView;
+    private TextView userScoreTextView;
     private TextView cpuScoreTextView;
-    private ImageView diceImageView;
     private TextView turnScoreTextView;
+    private ImageView diceImageView;
 
     private Button rollButton;
     private Button holdButton;
@@ -24,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
     private int cpuScore = 0;
     private int cpuTurnScore = 0;
 
-    Random random = new Random();
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playerScoreTextView = (TextView) findViewById(R.id.playerScoreTextView);
+        userScoreTextView = (TextView) findViewById(R.id.userScoreTextView);
         cpuScoreTextView = (TextView) findViewById(R.id.cpuScoreTextView);
         diceImageView = (ImageView) findViewById(R.id.diceImageView);
         turnScoreTextView = (TextView) findViewById(R.id.turnScoreTextView);
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    protected void reset(View view)
+    public void reset(View view)
     {
         diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice1));
         userTurnScore = 0;
@@ -48,100 +48,59 @@ public class MainActivity extends AppCompatActivity {
         userScore = 0;
         cpuScore = 0;
 
-        playerScoreTextView.setText(R.string.player_score + 0);
+        userScoreTextView.setText(R.string.player_score + 0);
         cpuScoreTextView.setText(R.string.cpu_score + 0);
         turnScoreTextView.setText(R.string.turnScore + 0);
     }
 
-    protected void rollDice(View view)
+    public void rollDice(View view)
     {
-        int roll = random.nextInt(6);
+        int roll = rollDice();
         switch (roll){
+            case 0: {
+
+                break;
+            }
             case 1: {
-                diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice1));
-                userTurnScore = 0;
-                turnScoreTextView.setText(R.string.turnScore + userTurnScore);
-                computerTurn();
+
+                break;
             }
             case 2: {
-                diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice2));
-                userTurnScore += 2;
-                turnScoreTextView.setText(R.string.turnScore + userTurnScore);
+
+                break;
             }
             case 3: {
-                diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice3));
-                userTurnScore += 3;
-                turnScoreTextView.setText(R.string.turnScore + userTurnScore);
+
+                break;
             }
             case 4: {
-                diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice4));
-                userTurnScore += 4;
-                turnScoreTextView.setText(R.string.turnScore + userTurnScore);
+
+                break;
             }
             case 5: {
-                diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice5));
-                userTurnScore += 5;
-                turnScoreTextView.setText(R.string.turnScore + userTurnScore);
-            }
-            case 6: {
-                diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice6));
-                userTurnScore += 6;
-                turnScoreTextView.setText(R.string.turnScore + userTurnScore);
-            }
 
+                break;
+            }
         }
     }
 
-    protected void holdTurn(View view)
+    private int rollDice()
     {
-        userScore += userTurnScore;
-        userTurnScore = 0;
-        turnScoreTextView.setText(R.string.turnScore + 0);
-
-        computerTurn();
-    }
-
-    private void computerTurn()
-    {
-        rollButton.setEnabled(false);
-        holdButton.setEnabled(false);
-
-        while(cpuScore < 20)
-        {
-            int roll = random.nextInt(6);
-            switch (roll){
-                case 1: {
-                    diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice1));
-                    cpuTurnScore = 0;
-                    turnScoreTextView.setText(R.string.turnScore + cpuTurnScore);
-                }
-                case 2: {
-                    diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice2));
-                    cpuTurnScore += 2;
-                    turnScoreTextView.setText(R.string.turnScore + cpuTurnScore);
-                }
-                case 3: {
-                    diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice3));
-                    cpuTurnScore += 3;
-                    turnScoreTextView.setText(R.string.turnScore + cpuTurnScore);
-                }
-                case 4: {
-                    diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice4));
-                    cpuTurnScore += 4;
-                    turnScoreTextView.setText(R.string.turnScore + cpuTurnScore);
-                }
-                case 5: {
-                    diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice5));
-                    cpuTurnScore += 5;
-                    turnScoreTextView.setText(R.string.turnScore + cpuTurnScore);
-                }
-                case 6: {
-                    diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice6));
-                    cpuTurnScore += 6;
-                    turnScoreTextView.setText(R.string.turnScore + cpuTurnScore);
-                }
-
-            }
+        int roll = random.nextInt(6);
+        switch(roll){
+            case 0: diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice1));
+                break;
+            case 1: diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice2));
+                break;
+            case 2: diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice3));
+                break;
+            case 3: diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice4));
+                break;
+            case 4: diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice5));
+                break;
+            case 5: diceImageView.setImageDrawable(getResources().getDrawable(R.drawable.dice6));
+                break;
         }
+        return roll;
     }
 }
